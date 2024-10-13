56cc7ba327b1d641573ac31f8a1a38e2a1a06d52
Manon Le Roux <manonleroux@mailbox.org>
2024-10-13 12:20:23 +0200

    test git connexion

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -19,10 +19,9 @@ with source_data as (
 
 select *
 from source_data
-where id=1 
 
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+where id is not null
